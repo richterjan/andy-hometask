53ecc23d0ffd2d15400613cf785a1d3feea614de
Jan Richter <jan.richter@actumdigital.com>
2024-07-15 17:55:09 +0200

    add spring

---
+++ src/main/java/actumdigital/andyhometask/AndyHometaskApplication.java
@@ -0,0 +1,13 @@
+package actumdigital.andyhometask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndyHometaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndyHometaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/actumdigital/andyhometask/AndyHometaskApplicationTests.java
@@ -0,0 +1,13 @@
+package actumdigital.andyhometask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndyHometaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
